3c839eb797147d36eca21430f8f48739515df91f
Han Tran <imhantran@gmail.com>
2021-04-07 16:25:58 +0700

    create nextjs app

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque magnam adipisci nulla itaque laboriosam odit aliquam quasi, quisquam sequi, aliquid iure. Odit fugiat alias porro debitis quia nostrum. Culpa, id?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque magnam adipisci nulla itaque laboriosam odit aliquam quasi, quisquam sequi, aliquid iure. Odit fugiat alias porro debitis quia nostrum. Culpa, id?</p>
+      </div>
+    </>
+  );
+}
+
+export default About;
